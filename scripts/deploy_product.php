ee9533dad124e97e5acb380fbd0cc6cdb4bdb47a
vivek-shamanna <vivek.hs@pantheon.io>
2024-10-07 12:57:53 +0530

    Adding the QS hooks

---
+++ scripts/deploy_product.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Quicksilver Debuging Output";
+echo "\n\n";
+echo "Quicksilver deploy_product Hook after stage output is below";
+echo "\n\n";
+echo "\n========= START PAYLOAD ===========\n";
+print_r($_POST);
+echo "\n========== END PAYLOAD ============\n";
+
+echo "\n------- START ENVIRONMENT ---------\n";
+$env = $_ENV;
+foreach ($env as $key => $value) {
+  if (preg_match('#(PASSWORD|SALT|AUTH|SECURE|NONCE|LOGGED_IN)#', $key)) {
+    $env[$key] = '[REDACTED]';
+  }
+}
+print_r($env);
+echo "\n-------- END ENVIRONMENT ----------\n";
